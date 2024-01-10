d7c92d6ebd018b98b3e6c6242d87ef7372d227b7
Mirre <14929246+MirreSJ@users.noreply.github.com>
2024-01-10 22:53:44 +0100

    remove empty col

--- LootGuide.Importer/Program.cs
+++ LootGuide.Importer/Program.cs
@@ -5,10 +5,10 @@ Console.WriteLine("Hello, World!");
 var loader = new FandomLoader();
 var requirements = await loader.LoadModulesAsync();
 var lines = new List<string>();
-lines.Add($"Module;Level;Count;Requirement;");
+lines.Add($"Module;Level;Count;Requirement");
 foreach (var requirement in requirements)
 {
-    lines.Add($"{requirement.Module};{requirement.Level};{requirement.Count:#,0.##};{requirement.Name};".Replace("\"", "'"));
+    lines.Add($"{requirement.Module};{requirement.Level};{requirement.Count:#,0.##};{requirement.Name}".Replace("\"", "'"));
 }
 File.WriteAllLines("../../../../latest-output.csv", lines);
 Console.WriteLine("done");
